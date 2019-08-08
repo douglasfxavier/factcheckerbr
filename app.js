f7295ae8e4ea5a882bac97d963a36cb2b8d943c7
Douglas Xavier <douglasf.xavier@gmail.com>
2019-08-08 20:09:32 -0300

    Alterando .gitignore; incluindo Procfile, Expressa e alterando app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var app = express()
+
+let port = process.env.PORT;
+
+app.get('/',function (req,res) {
+   res.send('Hello World!')
+});
+
+if (port == null || port == "") {
+    port = 8000;
+}
+
+app.listen(port, function() {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
