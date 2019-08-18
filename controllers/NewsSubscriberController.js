7d128c13ea40035188eb555dac92c27abcb3ec8c
Douglas Xavier <douglasf.xavier@gmail.com>
2019-08-18 19:02:30 -0300

    Inclui metodo para tratamento de post para o endpoint news/susbscribe

--- controllers/NewsSubscriberController.js
+++ controllers/NewsSubscriberController.js
@@ -1,6 +1,6 @@
-exports.subscribe = function (news_url) {
+exports.subscribe = function (req, res) {
     let reviewsCollection = app.locals.reviews;
 
-    
-
+    reviews = reviewsCollection.find({"claimReview.itemReviewed.url":req.body.url});
+    res.send(reviews);
 };
