18c602bf57bafc9068074d02f86f99d027cf76fe
Douglas Xavier <douglasf.xavier@gmail.com>
2019-08-18 19:45:27 -0300

    Altera o metodo de resposta do subcribe

--- controllers/NewsSubscriberController.js
+++ controllers/NewsSubscriberController.js
@@ -1,6 +1,6 @@
 exports.subscribe = function (req, res) {
     let reviewsCollection = app.locals.reviews;
 
-    reviews = reviewsCollection.find({"claimReview.itemReviewed.url":req.body.url});
-    res.send(reviews);
+    //reviews = reviewsCollection.find({"claimReview.itemReviewed.url":req.body.url});
+    res.json({teste: "testado"});
 };
