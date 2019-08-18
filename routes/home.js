84ccb16c06f61e007419d832abcdea6b7e5a7b27
Douglas Xavier <douglasf.xavier@gmail.com>
2019-08-18 15:22:53 -0300

    Inclui controler para cliente RabbitMQ implementando abordagem publish/subscribe

--- routes/home.js
+++ routes/home.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 let home = require('../controllers/HomeController');
 let review = require('../controllers/ReviewController');
+let subscriber = require('../controllers/NewsSubscriberController');
 
 // Home routes
 router.get('/',home.get_landingpage);
@@ -10,6 +11,7 @@ router.post('/review/create',home.create_review);
 
 // Review routes
 router.post('/review/sucesso',review.save_review);
+router.post('/news/subscribe',subscriber.subscribe);
 
 module.exports = router;
 
