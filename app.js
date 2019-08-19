c5ae890711e4877ff709a8644424eb08f0d25e3d
Douglas Xavier <douglasf.xavier@gmail.com>
2019-08-19 02:03:58 -0300

    Inclui estilização do front-end

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'resources')));
 app.use('/', homeRouter);
-
 module.exports = app;
