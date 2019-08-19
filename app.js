e2c7db56338185d2c86157315f71d966cf597825
Douglas Xavier <douglasf.xavier@gmail.com>
2019-08-19 04:08:31 -0300

    Inclui cors no app

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var path = require('path');
+var cors = require('cors');
 var homeRouter = require('./routes/home');
 var app = express();
 
+app.use(cors());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'resources')));
 app.use('/', homeRouter);
+
 module.exports = app;
